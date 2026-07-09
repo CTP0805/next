62f4b121e745dcd345e9bee85a9b6c354879af64
Tingan <syeak123@gmail.com>
2026-07-09 16:53:25 +0800

    加入熱門項目卡片

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,15 @@ import HeroSection from "@/components/HeroSection";
 import PopularDestinations from "@/components/PopularDestinations";
 import Footer from "@/components/Footer";
 import IconMenu from "@/components/IconMenu";
+import ExperienceCard from "@/components/ExperienceCard";
+
 export default function Home() {
   return (
     <>
       <Header />
       <HeroSection />
       <PopularDestinations />
+      <ExperienceCard />
       <Footer />
     </>
   );
