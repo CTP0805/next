5ccb4db86354227d7f4a8beb8a8e493952efe318
Tingan <syeak123@gmail.com>
2026-07-13 15:16:09 +0800

    留言區元件

---
+++ components/ReviewSection.tsx
@@ -0,0 +1,76 @@
+// components/ReviewCard.tsx
+export const columns = [
+  [
+    { type: "review", id: 1 },
+    { type: "image", id: 2 },
+  ],
+  [
+    { type: "image", id: 3 },
+    { type: "review", id: 4 },
+  ],
+  [
+    { type: "review", id: 6 },
+    { type: "image", id: 7 },
+  ],
+  [
+    { type: "image", id: 8 },
+    { type: "review", id: 9 },
+  ],
+];
+export function ReviewCard() {
+  return (
+    <div className="rounded-3xl bg-gray-100 p-6 shadow-sm">
+      <div className="flex items-center gap-3">
+        <div className="h-12 w-12 rounded-full bg-gray-300" />
+
+        <div>
+          <p className="">Sarah W.</p>
+          ⭐⭐⭐⭐⭐
+        </div>
+      </div>
+
+      <p className="mt-4 text-gray-600">「這不是觀光行程，而是在交朋友。」</p>
+    </div>
+  );
+}
+// components/ImageCard.tsx
+
+import Image from "next/image";
+
+export function ImageCard() {
+  return (
+    <div className="relative h-96 overflow-hidden rounded-3xl">
+      <Image
+        src="/images/carousel1.jpg"
+        alt=""
+        fill
+        className="rounded-3xl object-cover"
+      />
+    </div>
+  );
+}
+
+// app/page.tsx
+
+export default function Home() {
+  return (
+    <section className="mx-auto max-w-7xl px-6 py-20">
+      <div className="grid grid-cols-1 gap-8 md:grid-cols-2 xl:grid-cols-4">
+        {columns.map((column, index) => (
+          <div
+            key={index}
+            className={`space-y-8 ${index % 2 === 0    ? "md:pt-24" : ""}`}
+          >
+            {column.map((item) =>
+              item.type === "review" ? (
+                <ReviewCard key={item.id} />
+              ) : (
+                <ImageCard key={item.id} />
+              ),
+            )}
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
