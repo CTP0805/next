51c6a63bd99a0f190e35cec061c80ca42a7e36bd
Tingan <syeak123@gmail.com>
2026-07-19 23:12:18 +0800

    暫時刪除聊天室元件

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import HeroSection from "@/components/HeroSection";
 import PopularDestinations from "@/components/PopularDestinations";
 import ExperienceCard from "@/components/ExperienceCard";
 import ReviewSection from "@/components/ReviewSection";
-import ChatWidget from "@/components/ChatWidget";
 export default function Home() {
   return (
     <>
@@ -10,7 +9,6 @@ export default function Home() {
       <PopularDestinations />
       <ExperienceCard />
       <ReviewSection />
-      <ChatWidget />
     </>
   );
 }
