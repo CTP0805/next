18dbec2d8e400d70cacdc32ad3f787e6c77d7a88
Tingan <syeak123@gmail.com>
2026-07-21 15:37:41 +0800

    新增Features元件

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import HeroSection from "@/components/HeroSection";
 import PopularDestinations from "@/components/PopularDestinations";
 import ExperienceCard from "@/components/ExperienceCard";
 import ReviewSection from "@/components/ReviewSection";
+import Features from "@/components/Features";
 export default function Home() {
   return (
     <>
       <HeroSection />
       <PopularDestinations />
       <ExperienceCard />
+      <Features />
       <ReviewSection />
     </>
   );
